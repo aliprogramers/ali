6fdf94bec88b999d2294551d056204ff4a54df93
aliprogramers <138463569+aliprogramers@users.noreply.github.com>
2023-07-03 16:42:41 +0600

    sdafsa
    df

---
+++ 1/script.js
@@ -0,0 +1,40 @@
+// console.log(navigator.userAgent);
+// console.log(navigator.platform);
+// console.log(location.href);
+// while(true){
+// location.href = 'https://www.youtube.com/watch?v=FM7Z-Xq8Drc'
+
+// }
+let documetn= document.body
+// let documents = documetn.childNodes
+// console.log(documents);
+// for(let node of documetn){
+    // console.log(node);
+// }
+
+// let s = documetn.previousSibling
+// let s2 = documetn.nextSibling
+// let s3 = documetn.parentNode
+// console.log(s);
+// console.log(s2);
+// console.log(s3);
+// let buchld = document.childNodes;
+// console.log(buchld);
+// let first = documetn.firstElementChild;
+// console.log(first);
+// let aset = documetn.lastElementChild
+// console.log(aset);
+// let quaer = documetn.querySelectorAll('.hellos'
+// let st =  documetn.querySelectorAll('p')
+// for(let itme of st){
+//     console.log(itme);
+// }
+// let buid = document.getElementById('he joker')
+// console.log(buid);
+// let y = document.querySelector('.hellos')
+// console.log(y);
+// let tag = document.getElementsByTagName('div')
+// console.log(tag);
+// let classs = document.getElementsByClassName('hellos')
+// console.log(classs);
+ 
